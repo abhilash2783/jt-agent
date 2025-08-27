f5fa9cac51ae0b6c12ce58377d7cec7fd13752ae
abhilash-crystawall <abhilash.r@crystawall.com>
2025-08-27 17:44:41 +0530

    Add JIRA_SECRET to env.ts and include @types/node-fetch in devDependencies

--- src/env.ts
+++ src/env.ts
@@ -6,3 +6,4 @@ export const JIRA_BASE_URL = process.env.JIRA_BASE_URL || '';
 export const JIRA_USER = process.env.JIRA_USER || '';
 export const JIRA_TOKEN = process.env.JIRA_TOKEN || '';
 export const JIRA_PROJECT_KEY = process.env.JIRA_PROJECT_KEY || '';
+export const JIRA_SECRET = process.env.JIRA_SECRET || '';
